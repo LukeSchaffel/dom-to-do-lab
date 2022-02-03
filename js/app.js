3776c9cac3fb84cb6777f746d73c14cb69fd53e0
LukeSchaffel <lukeschaffel@gmail.com>
2022-02-02 19:21:28 -0500

    start clear button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const inputField = document.getElementById('input-field')
 const btn = document.getElementById('submit-button')
 const list = document.getElementById('todo-list')
-
+const clrBtn = document.getElementById('clear-button')
 
 
 btn.addEventListener('click', function (evt) {
   let newLi = document.createElement('li')
   newLi.textContent = inputField.value
-  console.log(newLi)
+  newLi.className = 'list-item'
+  // console.log(newLi)
   if (newLi.textContent !== "") {
     list.appendChild(newLi)
   inputField.value= ""
@@ -15,5 +16,9 @@ btn.addEventListener('click', function (evt) {
   
 })
 
+clrBtn.addEventListener('click', function (evt) {
+  const lis = document.querySelector('.list-item')
+  console.log(lis)
+  list.removeChild(lis)
+})
 
-console.log(inputField)
